430b38b4e19a03cb1b0a70cd35f3a21bedf292af
Rakib Ahmed <rakib987@gmail.com>
2021-09-16 20:57:55 -0400

    added Resume Upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,11 @@ var dnaAnimation = bodymovin.loadAnimation({
 })
 
 const form2 = $(`.form2`);
+const form3 = $(`.form3`);
 form2.hide();
+form3.hide();
+
+$(`.form3 .back`).hide();
 
 const signIn = $(`.login`);
 signIn.hide();
@@ -25,22 +29,148 @@ const form1 = $(`.form1`);
 form1.on(`submit`,event => {
     event.preventDefault();
 
-    $(`.phoneNumber`).val() === `` ? alert(`Please enter a valid phone number`) : true;
-    isNaN($(`.phoneNumber`).val()) ? alert(`Please enter a valid phone number`) : true;
+    if ($(`.phoneNumber`).val() === ``) {
+        alert(`Please enter a valid phone number`);
+        return;
+    } else if (isNaN($(`.phoneNumber`).val())) {
+        alert(`Please enter a valid phone number`);
+        return; 
+    } else { // If all conditions are satisfied
+        const phoneNumber = $(`.phoneNumber`).val();
+        const language = $(`.languages`).val();
+        const position = $(`.position`).val();
+    
+        localStorage.setItem(`Phone Number`, phoneNumber)
+        localStorage.setItem(`Language`, language)
+        localStorage.setItem(`Position`, position)
+    
+        $(`.form1`).hide(750);
+        $(`.form2`).show(1500);
+        setTimeout(() => {
+            back.show();
+            signIn.fadeIn(1000);
+        },1500)
+        
+        form2.on(`submit`, event => {
+            event.preventDefault();
 
-    const phoneNumber = $(`.phoneNumber`).val();
-    const language = $(`.languages`).val();
-    const position = $(`.position`).val();
+            if ($(`.firstName`).val() === ``) {
+                alert(`Please enter a valid First Name`);
+                return;
+            } else if ($(`.lastName`).val() === ``) {
+                alert(`Please enter a valid Last Name`);
+                return;
+            } else if ($(`.emailAddress`).val() === ``) {
+                alert(`Please enter a valid Email Address`);
+                return; 
+            } else if ($(`.password`).val() === ``) {
+                alert(`Please enter a valid Password`);
+                return; 
+            } else if ($(`.company`).val() === ``) {
+                alert(`Please enter a valid Company Name... If you do not have one, put 'None'.`);
+                return; 
+            } else if ($(`.termsAgreement`).val() === false) {
+                alert(`You must agree to our terms of service and privacy policy`);
+                return; 
+            } else { // If all conditions are satisfied
+                const firstName = $(`.firstName`).val();
+                const lastName = $(`.lastName`).val();
+                const emailAddress = $(`.emailAddress`).val();
+                const password = $(`.password`).val();
+                const company = $(`.company`).val();
+                const termsAgreement = $(`.termsAgreement`).val();
+            
+                localStorage.setItem(`First Name`, firstName);
+                localStorage.setItem(`Last Name`, lastName);
+                localStorage.setItem(`Email Address`, emailAddress);
+                localStorage.setItem(`Password`, password);
+                localStorage.setItem(`Company`, company);
 
-    localStorage.setItem(`Phone Number`, phoneNumber)
-    localStorage.setItem(`Language`, language)
-    localStorage.setItem(`Position`, position)
+                console.log(termsAgreement);
+                $(`.form2 .back`).hide();
+            
+                $(`.form2`).hide(750);
+                $(`.form3`).show(1500);
+                setTimeout(() => {
+                    $(`.form3 .back`).show();
+                    signIn.fadeOut(1000);
+                },1500)
 
-    $(`.form1`).hide(750);
-    $(`.form2`).show(1500);
-    setTimeout(() => {
-        back.show();
-        signIn.fadeIn(1000);
-    },1500)
+                // User File Handler
+                var actualInput = $(`.fileInput`);
+                actualInput.change(fileHandler);
+
+                // On File Upload Button Click
+                var dropZone = document.querySelector(`.dropZone`);
+                dropZone.addEventListener(`click`, event => {
+                    actualInput.click();
+                })
+
+                dropZone.addEventListener(`drop`, fileHandler);
+
+                    dropZone.addEventListener("dragover", (e) => {
+                        e.preventDefault();
+                        dropZone.classList.add("drop-zone--over");
+                    });
+
+                    ["dragleave", "dragend"].forEach((type) => {
+                        dropZone.addEventListener(type, (e) => {
+                        dropZone.classList.remove("drop-zone--over");
+                        });
+                    });
+
+                    dropZone.addEventListener("drop", (e) => {
+                        e.preventDefault();
+
+                        if (e.dataTransfer.files.length) {
+                        actualInput.files = e.dataTransfer.files;
+                        }
+
+                        dropZone.classList.remove("drop-zone--over");
+                    });
+
+            }
+        })
+        
+    }
 
 })
+
+// Functions
+function fileHandler(event,fileList) {
+    event.preventDefault();
+
+    fileList = event.dataTransfer.files;
+    console.log(event.dataTransfer.files);
+
+        console.log(fileList);
+        var filesContent = $(`<div class="files"></div>`);
+
+        // Generating Elements for Each File
+        Object.values(fileList).forEach((file,index) => {
+            console.log(file);
+            iconType = (icon => {
+                icon = file.type;
+                switch (icon) {
+                    case `audio/mp3`:
+                    return icon = `<i class="fas fa-file-audio"></i>`;
+                    case `video/mp4`: 
+                    return icon = `<i class="fas fa-file-video"></i>`;
+                    case `audio/mpeg`:
+                    return icon = `<i class="fas fa-file-audio"></i>`;
+                    default:
+                    return icon = `<i class="fas fa-file-pdf"></i>`;
+                } // Generating Icon Based on Which File Type
+                return icon;
+            }) // Generating File Card Elements
+            var fileCard = $(`
+                <div class="fileCard">
+                    <div class="fileIcon">${iconType()}</div>
+                    <div class="fileName">${file.name}</div>
+                    <div class="fileType">${file.type}</div>
+                </div>
+            `);
+            filesContent.append(fileCard);
+        })
+        form3.append(filesContent);
+}
